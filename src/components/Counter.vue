3c33b61367fa3e6be66a704c49ef9e813f86fabd
Pavel Tkachuk <pavelt0309@gmail.com>
2024-04-24 22:28:42 +0200

    feat: project cleanup; add bootstrap and sass; implement couner component

---
+++ src/components/Counter.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const counter = ref(0)
+
+const increment = () => counter.value++;
+const decrement = () => counter.value--;
+</script>
+
+<template>
+    <div class="container counter">
+        <div class="row align-items-center row-cols-auto">
+            <div class="col">
+                <button class="btn btn-primary" @click="decrement">
+                    -
+                </button>
+            </div>
+            <div class="col counter__value">
+                {{ counter }}
+            </div>
+            <div class="col">
+                <button class="btn btn-primary" @click="increment">+ </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.counter {
+    &__value {
+        font-weight: bold;
+        font-size: large;
+    }
+}
+</style>
